7b60cf58dc458c0d7dc73811cd41faf7b5cae6b4
abby <abbyhsiung@gmail.com>
2019-03-22 14:51:12 -0400

    many changes happened

--- static/js/task.js
+++ static/js/task.js
@@ -82,21 +82,21 @@ var Curiosity = function() {
                    // {src: "static/videos/candle.mp4", type: "video/mp4", answer: "Candle"},
                    // {src: "static/videos/cappuccino.mp4", type: "video/mp4", answer: "Cappuccino"},
                    // {src: "static/videos/car.mp4", type: "video/mp4", answer: "Car"},
-                   // {src: "static/videos/carrots.mp4", type: "video/mp4", answer: "Carrots"},
-                   // {src: "static/videos/cat.mp4", type: "video/mp4", answer: "Cat"},
+                   {src: "static/videos/carrots.mp4", type: "video/mp4", answer: "Carrots"},
+                   {src: "static/videos/cat.mp4", type: "video/mp4", answer: "Cat"},
                    // {src: "static/videos/chef.mp4", type: "video/mp4", answer: "Chef"},
                    // {src: "static/videos/constructionworker.mp4", type: "video/mp4", answer: "Construction Worker"},
-                   // {src: "static/videos/corn.mp4", type: "video/mp4", answer: "Corn"},
+                   {src: "static/videos/corn.mp4", type: "video/mp4", answer: "Corn"},
                    // {src: "static/videos/crab.mp4", type: "video/mp4", answer: "Crab"},
-                   // {src: "static/videos/cupcake.mp4", type: "video/mp4", answer: "Cupcake"},
+                   {src: "static/videos/cupcake.mp4", type: "video/mp4", answer: "Cupcake"},
                    // {src: "static/videos/dandelion.mp4", type: "video/mp4", answer: "Dandelion"},
-                   // {src: "static/videos/deer.mp4", type: "video/mp4", answer: "Deer"},
+                   {src: "static/videos/deer.mp4", type: "video/mp4", answer: "Deer"},
                    // {src: "static/videos/dog.mp4", type: "video/mp4", answer: "Dog"},
                    // {src: "static/videos/dolphin.mp4", type: "video/mp4", answer: "Dolphin"},
                    // {src: "static/videos/drum.mp4", type: "video/mp4", answer: "Drum"},
-                   // {src: "static/videos/drummer.mp4", type: "video/mp4", answer: "Drummer"},
+                   {src: "static/videos/drummer.mp4", type: "video/mp4", answer: "Drummer"},
                    // {src: "static/videos/eel.mp4", type: "video/mp4", answer: "Eel"},
-                   // {src: "static/videos/elephant.mp4", type: "video/mp4", answer: "Elephant"},
+                   {src: "static/videos/elephant.mp4", type: "video/mp4", answer: "Elephant"},
                    // {src: "static/videos/fan.mp4", type: "video/mp4", answer: "Fan"},
                    // {src: "static/videos/faucet.mp4", type: "video/mp4", answer: "Faucet"},
                    // {src: "static/videos/flamingo.mp4", type: "video/mp4", answer: "Flamingo"},
@@ -120,7 +120,7 @@ var Curiosity = function() {
                    // {src: "static/videos/horse.mp4", type: "video/mp4", answer: "Horse"},
                    // {src: "static/videos/hotairballoon.mp4", type: "video/mp4", answer: "Hot Air Balloon"},
                    // {src: "static/videos/hourglass.mp4", type: "video/mp4", answer: "Hourglass"},
-                   // {src: "static/videos/iceberg.mp4", type: "video/mp4", answer: "Iceberg"},
+                   {src: "static/videos/iceberg.mp4", type: "video/mp4", answer: "Iceberg"},
                    // {src: "static/videos/icecream.mp4", type: "video/mp4", answer: "Ice Cream"},
                    // {src: "static/videos/jellyfish.mp4", type: "video/mp4", answer: "Jelly Fish"},
                    // {src: "static/videos/kangaroo.mp4", type: "video/mp4", answer: "Kangaroo"},
@@ -128,11 +128,11 @@ var Curiosity = function() {
                    // {src: "static/videos/lamp.mp4", type: "video/mp4", answer: "Lamp"},
                    // {src: "static/videos/laptop.mp4", type: "video/mp4", answer: "Laptop"},
                    // {src: "static/videos/lightbulb.mp4", type: "video/mp4", answer: "Light Bulb"},
-                   // {src: "static/videos/lighthouse.mp4", type: "video/mp4", answer: "Light House"},
+                   {src: "static/videos/lighthouse.mp4", type: "video/mp4", answer: "Light House"},
                    // {src: "static/videos/lion.mp4", type: "video/mp4", answer: "Lion"},
-                   // {src: "static/videos/lizard.mp4", type: "video/mp4", answer: "Lizard"},
+                   {src: "static/videos/lizard.mp4", type: "video/mp4", answer: "Lizard"},
                    // {src: "static/videos/llama.mp4", type: "video/mp4", answer: "Llama"},
-                   // {src: "static/videos/lungs.mp4", type: "video/mp4", answer: "Lungs"},
+                   {src: "static/videos/lungs.mp4", type: "video/mp4", answer: "Lungs"},
                    // {src: "static/videos/microphone.mp4", type: "video/mp4", answer: "Microphone"},
                    // {src: "static/videos/microscope.mp4", type: "video/mp4", answer: "Microscope"},
                    // {src: "static/videos/moose.mp4", type: "video/mp4", answer: "Moose"},
@@ -141,46 +141,46 @@ var Curiosity = function() {
                    // {src: "static/videos/nut.mp4", type: "video/mp4", answer: "Nut"},
                    // {src: "static/videos/okra.mp4", type: "video/mp4", answer: "Okra"},
                    // {src: "static/videos/orca.mp4", type: "video/mp4", answer: "Orca"},
-                   // {src: "static/videos/owl.mp4", type: "video/mp4", answer: "Owl"},
+                   {src: "static/videos/owl.mp4", type: "video/mp4", answer: "Owl"},
                    // {src: "static/videos/paintbrush.mp4", type: "video/mp4", answer: "Paintbrush"},
-                   // {src: "static/videos/palmtree.mp4", type: "video/mp4", answer: "Palm Tree"},
-                   // {src: "static/videos/pancakes.mp4", type: "video/mp4", answer: "Pancakes"},
+                   {src: "static/videos/palmtree.mp4", type: "video/mp4", answer: "Palm Tree"},
+                   {src: "static/videos/pancakes.mp4", type: "video/mp4", answer: "Pancakes"},
                    // {src: "static/videos/panda.mp4", type: "video/mp4", answer: "Panda"},
                    // {src: "static/videos/papaya.mp4", type: "video/mp4", answer: "Papaya"},
-                   // {src: "static/videos/pear.mp4", type: "video/mp4", answer: "Pear"},
-                   // {src: "static/videos/pencil.mp4", type: "video/mp4", answer: "Pencil"},
+                   {src: "static/videos/pear.mp4", type: "video/mp4", answer: "Pear"},
+                   {src: "static/videos/pencil.mp4", type: "video/mp4", answer: "Pencil"},
                    // {src: "static/videos/penguin.mp4", type: "video/mp4", answer: "Penguin"},
                    // {src: "static/videos/phonograph.mp4", type: "video/mp4", answer: "Phonograph"},
-                   // {src: "static/videos/pig.mp4", type: "video/mp4", answer: "Pig"},
+                   {src: "static/videos/pig.mp4", type: "video/mp4", answer: "Pig"},
                    // {src: "static/videos/pineapple.mp4", type: "video/mp4", answer: "Pineapple"},
                    // {src: "static/videos/pinecone.mp4", type: "video/mp4", answer: "Pinecone"},
-                   // {src: "static/videos/pinetree.mp4", type: "video/mp4", answer: "Pine Tree"},
+                   {src: "static/videos/pinetree.mp4", type: "video/mp4", answer: "Pine Tree"},
                    // {src: "static/videos/pipe.mp4", type: "video/mp4", answer: "Pipe"},
                    // {src: "static/videos/pizza.mp4", type: "video/mp4", answer: "Pizza"},
                    // {src: "static/videos/pot.mp4", type: "video/mp4", answer: "Pot"},
-                   // {src: "static/videos/rabbit.mp4", type: "video/mp4", answer: "Rabbit"},
-                   // {src: "static/videos/raccoon.mp4", type: "video/mp4", answer: "Raccoon"},
-                   // {src: "static/videos/ram.mp4", type: "video/mp4", answer: "Ram"},
+                   {src: "static/videos/rabbit.mp4", type: "video/mp4", answer: "Rabbit"},
+                   {src: "static/videos/raccoon.mp4", type: "video/mp4", answer: "Raccoon"},
+                   {src: "static/videos/ram.mp4", type: "video/mp4", answer: "Ram"},
                    // {src: "static/videos/rhino.mp4", type: "video/mp4", answer: "Rhino"},
                    // {src: "static/videos/rooster.mp4", type: "video/mp4", answer: "Rooster"},
                    // {src: "static/videos/rose.mp4", type: "video/mp4", answer: "Rose"},
-                   // {src: "static/videos/sailboat.mp4", type: "video/mp4", answer: "Sailboat"},
-                   // {src: "static/videos/saxophone.mp4", type: "video/mp4", answer: "Saxophone"},
+                   {src: "static/videos/sailboat.mp4", type: "video/mp4", answer: "Sailboat"},
+                   {src: "static/videos/saxophone.mp4", type: "video/mp4", answer: "Saxophone"},
                    // {src: "static/videos/scale.mp4", type: "video/mp4", answer: "Scale"},
                    // {src: "static/videos/scissors.mp4", type: "video/mp4", answer: "Scissors"},
                    // {src: "static/videos/scorpion.mp4", type: "video/mp4", answer: "Scorpion"},
                    // {src: "static/videos/seahorse.mp4", type: "video/mp4", answer: "Seahorse"},
-                   // {src: "static/videos/seal.mp4", type: "video/mp4", answer: "Seal"},
+                   {src: "static/videos/seal.mp4", type: "video/mp4", answer: "Seal"},
                    // {src: "static/videos/shark.mp4", type: "video/mp4", answer: "Shark"},
                    // {src: "static/videos/shell.mp4", type: "video/mp4", answer: "Shell"},
                    // {src: "static/videos/skateboard.mp4", type: "video/mp4", answer: "Skateboard"},
                    // {src: "static/videos/sloth.mp4", type: "video/mp4", answer: "Sloth"},
-                   // {src: "static/videos/snail.mp4", type: "video/mp4", answer: "Snail"},
+                   {src: "static/videos/snail.mp4", type: "video/mp4", answer: "Snail"},
                    // {src: "static/videos/sneaker.mp4", type: "video/mp4", answer: "Sneaker"},
                    // {src: "static/videos/squid.mp4", type: "video/mp4", answer: "Squid"},
-                   // {src: "static/videos/squirrel.mp4", type: "video/mp4", answer: "Squirrel"},
-                   // {src: "static/videos/succulent.mp4", type: "video/mp4", answer: "Succulent"},
-                   // {src: "static/videos/surfer.mp4", type: "video/mp4", answer: "Surfer"},
+                   {src: "static/videos/squirrel.mp4", type: "video/mp4", answer: "Squirrel"},
+                   {src: "static/videos/succulent.mp4", type: "video/mp4", answer: "Succulent"},
+                   {src: "static/videos/surfer.mp4", type: "video/mp4", answer: "Surfer"},
                    // {src: "static/videos/swordfish.mp4", type: "video/mp4", answer: "Swordfish"},
                    // {src: "static/videos/taco.mp4", type: "video/mp4", answer: "taco"},
                    // {src: "static/videos/television.mp4", type: "video/mp4", answer: "Television"},
@@ -192,7 +192,7 @@ var Curiosity = function() {
                    // {src: "static/videos/walrus.mp4", type: "video/mp4", answer: "Walrus"},
                    // {src: "static/videos/watermelon.mp4", type: "video/mp4", answer: "Watermelon"},
                    // {src: "static/videos/wave.mp4", type: "video/mp4", answer: "Wave"},
-                   {src: "static/videos/whale.mp4", type: "video/mp4", answer: "Whale"},
+                   // {src: "static/videos/whale.mp4", type: "video/mp4", answer: "Whale"},
                    // {src: "static/videos/wineglass.mp4", type: "video/mp4", answer: "Wine Glass"},
                    // {src: "static/videos/wolf.mp4", type: "video/mp4", answer: "Wolf"},
                    // {src: "static/videos/yogi.mp4", type: "video/mp4", answer: "Yogi"},
@@ -243,32 +243,32 @@ var Curiosity = function() {
             },
             nextVideo() {
                 this.data.push({
+                    trialNumber: this.videoIndex,
+                    trialAnswer: document.getElementById( "video" ).src,
                     submitTimeList: this.submitTimeList,
-                    videoGuess: this.currentGuess,
+                    lastVideoGuess: this.currentGuess,
                     percentageSubmitList: this.percentageSubmitList,
                     guessList: this.guessList,
                     curiosityValue: this.curiosityValue,
                     speedValue: this.speedValue,
-                    // correct: this.correct,
                     finalGuess: this.finalGuess,
                 });
 
                 psiTurk.recordTrialData({
-                        submitTimeList: this.submitTimeList,
-                        videoGuess: this.currentGuess,
-                        percentageSubmitList: this.percentageSubmitList,
-                        guessList: this.guessList,
-                        curiosityValue: this.curiosityValue,
-                        speedValue: this.speedValue,
-                        // correct: this.correct,
-                        finalGuess: this.finalGuess,
-                    }
-                );
+                    trialNumber: this.videoIndex,
+                    trialAnswer: document.getElementById( "video" ).src,
+                    submitTimeList: this.submitTimeList,
+                    lastVideoGuess: this.currentGuess,
+                    percentageSubmitList: this.percentageSubmitList,
+                    guessList: this.guessList,
+                    curiosityValue: this.curiosityValue,
+                    speedValue: this.speedValue,
+                    finalGuess: this.finalGuess,
+                });
 
                 this.videoIndex++;
                 console.log(this.data);
                 console.log("The Current video index is", this.videoIndex);
-                console.log(this.videoList[this.videoIndex]);
                 this.submitTime = 0;
                 this.percentageSubmit = 0;
                 this.submitTimeList = [];
@@ -289,19 +289,19 @@ var Curiosity = function() {
                     currentview = new Questionnaire();
                 } else {
                     myVideo.src = this.videoList[this.videoIndex].src;
-                    document.getElementById("videoGuess").style.display = "block";
+                    document.getElementById("videoGuess").style.display = "inline";
                     document.getElementById("displayGuess").style.display = "none";
                     document.getElementById("curiosityMeasure").style.display = "none";
+                    this.setFocus();
                 }
+
             },
             continueToStart() {
                 var myVideo = document.getElementById("video");
                 myVideo.src = this.videoList[this.videoIndex].src;
                 document.getElementById("startExp").style.display = "none"
                 document.getElementById("video1").style.display = "block";
-                console.log(this.videoList);
                 console.log("this is the video index when we hit continue to start: ", this.videoIndex);
-                console.log(this.videoList[this.videoIndex])
             },
             testInput() {
                 var myVideo = document.getElementById("video");
@@ -335,6 +335,7 @@ var Curiosity = function() {
                 var keyCode = event.keyCode;
                 if (keyCode == 13) {
                     this.checkVideoGuess(this.videoGuess);
+
                     if (this.validVideoGuess == 1) {
                         this.finalGuess = this.videoGuess;
                         // this.guessList.push(this.finalGuess);
@@ -349,6 +350,8 @@ var Curiosity = function() {
                 document.getElementById("curiosityMeasure").style.display = "block";
                 var myVideo = document.getElementById("video");
                 myVideo.pause();
+                document.getElementById("finalGuess").focus();
+                document.getElementById("finalGuess").select();
             },
             sliderFxn_curiosity() {
                 var slider = document.getElementById("slider");
@@ -368,6 +371,13 @@ var Curiosity = function() {
                 else {
                     this.validVideoGuess = 1;
                 }
+            },
+            setFocus() {
+                setTimeout(()=>{
+                    document.getElementById("videoGuess").focus();
+                }, 50)
+
+
             },
         }
     });
@@ -406,9 +416,7 @@ var Questionnaire = function() {
                 // psiTurk.computeBonus('compute_bonus', function(){
                 // 	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
                 // });
-
-
-			},
+            },
 			error: prompt_resubmit
 		});
 	};
@@ -439,31 +447,6 @@ var currentview;
 /*******************
  * Run Task
  ******************/
-
-/*$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "static/resources/chances.csv",
-        dataType: "text",
-        success: function(data) {
-            load(data);
-        },
-        error: function(req, status, error){
-            $("body").html("<p>" + error + "</p>");
-        }
-    });
-});*/
-
-/*var load = function(data) {
-    $(window).load(function () {
-        psiTurk.doInstructions(
-            instructionPages, // a list of pages you want to display in sequence
-            function () {
-                currentview = new Mousetrack(data);
-            } // what you want to do when you are done with instructions
-        );
-    });
-};*/
 
 $(window).load(function () {
     psiTurk.doInstructions(
--- util/psi-test.sh
+++ util/psi-test.sh
@@ -1,8 +1,16 @@
+git checkout -- config.txt
+util/setupConfig.sh
+
 if ! screen -list | grep -q exp; then     # run bash script
 	screen -dmS exp bash
 fi
 
-echo "Starting up server..."
 screen -S exp -p 0 -X stuff "util/restart-debug.sh$(echo -ne '\r')"
-echo "Server started."
 
+while psiturk -e status | grep -q off
+do
+	echo "Waiting..."
+    sleep 1
+done
+
+echo "Server on."
